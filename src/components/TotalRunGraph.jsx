d492e45e293dfbfcfbbd7f60bac31aa29eaa7ec2
Jamie Tardi <jamie.tardi@gmail.com>
2021-04-30 07:48:00 +0100

    added total distance chart for 30 days

--- src/components/TotalRunGraph.jsx
+++ src/components/TotalRunGraph.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card } from 'react-bootstrap';
+import { Line } from 'react-chartjs-2';
 
-const TotalRunGraph = () => {
-	return <div></div>;
+const TotalRunGraph = ({ activities }) => {
+	// const [runDistance, setRunDistance] = useState([]);
+	let runDistance = [];
+	let totalRuns = [];
+
+	try {
+		activities.map((run) => {
+			runDistance.push((run.distance * 0.000621371192).toFixed(2));
+		});
+		for (let i = 1; i < activities.length; i++) {
+			totalRuns.push(i);
+		}
+	} catch (e) {
+		console.log(e);
+	}
+	const data = {
+		labels: totalRuns,
+		datasets: [
+			{
+				label: 'Miles run previous 30 runs',
+				data: runDistance,
+				fill: false,
+				backgroundColor: '#2D76EB',
+				borderColor: '#07F523',
+			},
+		],
+	};
+
+	return <div>{activities ? <Line data={data} /> : '...Loading'}</div>;
 };
 
 export default TotalRunGraph;
