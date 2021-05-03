f0f6eb7e817dd2cf6fe80b10a24a3979403c9c43
Jamie Tardi <jamie.tardi@gmail.com>
2021-05-03 16:09:19 +0100

    creating comparision data for weeks

--- src/components/TotalRunGraph.jsx
+++ src/components/TotalRunGraph.jsx
@@ -6,17 +6,32 @@ const TotalRunGraph = ({ activities }) => {
 	// const [runDistance, setRunDistance] = useState([]);
 	let runDistance = [];
 	let totalRuns = [];
+	let lastSevenDaysRunTotal = 0;
 
+	//Total Run distance previous 30 days
 	try {
 		activities.map((run) => {
 			runDistance.push((run.distance * 0.000621371192).toFixed(2));
 		});
-		for (let i = 1; i < activities.length; i++) {
+		for (let i = 0; i < activities.length; i++) {
 			totalRuns.push(i);
 		}
 	} catch (e) {
 		console.log(e);
 	}
+
+	//Comparision between last 7 runs and the previous 7 before that
+	try {
+		for (let i = 0; i < 7; i++) {
+			lastSevenDaysRunTotal += parseInt(
+				activities[i].distance * 0.000621371192,
+			);
+		}
+		console.log(lastSevenDaysRunTotal);
+	} catch (e) {
+		console.log(e);
+	}
+
 	const data = {
 		labels: totalRuns,
 		datasets: [
@@ -33,7 +48,12 @@ const TotalRunGraph = ({ activities }) => {
 	return (
 		<div className='container'>
 			{activities ? (
-				<Line data={data} />
+				<Card>
+					<Card.Header>
+						<Line data={data} />
+					</Card.Header>
+					<Card.Body>Runs for the past 30 days</Card.Body>
+				</Card>
 			) : (
 				<Spinner animation='grow' variant='success' />
 			)}
