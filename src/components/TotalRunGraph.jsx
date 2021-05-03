5d778bf25a35bae9bea4e555a38fe596dda06e89
Jamie Tardi <jamie.tardi@gmail.com>
2021-05-03 16:33:03 +0100

    implemented the doughnut chart

--- src/components/TotalRunGraph.jsx
+++ src/components/TotalRunGraph.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { Card, Spinner } from 'react-bootstrap';
-import { Line } from 'react-chartjs-2';
+import { Line, Doughnut } from 'react-chartjs-2';
 
 const TotalRunGraph = ({ activities }) => {
 	// const [runDistance, setRunDistance] = useState([]);
 	let runDistance = [];
 	let totalRuns = [];
 	let lastSevenDaysRunTotal = 0;
+	let previousWeekRunTotal = 0;
 
 	//Total Run distance previous 30 days
 	try {
@@ -22,12 +23,18 @@ const TotalRunGraph = ({ activities }) => {
 
 	//Comparision between last 7 runs and the previous 7 before that
 	try {
-		for (let i = 0; i < 7; i++) {
-			lastSevenDaysRunTotal += parseInt(
-				activities[i].distance * 0.000621371192,
-			);
+		for (let i = 0; i < 14; i++) {
+			if (i < 7) {
+				lastSevenDaysRunTotal += parseInt(
+					activities[i].distance * 0.000621371192,
+				);
+			} else {
+				previousWeekRunTotal += parseInt(
+					activities[i].distance * 0.000621371192,
+				);
+			}
 		}
-		console.log(lastSevenDaysRunTotal);
+		console.log(previousWeekRunTotal);
 	} catch (e) {
 		console.log(e);
 	}
@@ -45,15 +52,39 @@ const TotalRunGraph = ({ activities }) => {
 		],
 	};
 
+	const dataComp = {
+		labels: ['Last Seven Days', 'Previous 7 days'],
+		datasets: [
+			{
+				label: 'Total miles run current week vs last week',
+				data: [lastSevenDaysRunTotal, previousWeekRunTotal],
+				fill: false,
+				backgroundColor: ['#ce0415', '#f5f107'],
+				borderColor: 'black',
+			},
+		],
+	};
+
 	return (
 		<div className='container'>
 			{activities ? (
-				<Card>
-					<Card.Header>
-						<Line data={data} />
-					</Card.Header>
-					<Card.Body>Runs for the past 30 days</Card.Body>
-				</Card>
+				<div>
+					<Card>
+						<Card.Header>
+							<Line data={data} />
+						</Card.Header>
+						<Card.Body>Runs for the past 30 activities.</Card.Body>
+					</Card>
+					<Card>
+						<Card.Header>
+							<Doughnut data={dataComp} />
+						</Card.Header>
+						<Card.Body>
+							Total distance tlast 7 runs compared to the distance the previous
+							7 runs.
+						</Card.Body>
+					</Card>
+				</div>
 			) : (
 				<Spinner animation='grow' variant='success' />
 			)}
