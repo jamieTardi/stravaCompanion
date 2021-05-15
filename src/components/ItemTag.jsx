6d1420877385175a3ee7ff999755dd2576a23767
Jamie Tardi <jamie.tardi@gmail.com>
2021-05-15 16:59:45 +0100

    adding styles to the filter

--- src/components/ItemTag.jsx
+++ src/components/ItemTag.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import cross from '../assets/icons/cross.svg';
 
 const ItemTag = () => {
 	return (
-		<div className='item-tag-container d-flex justify-content-center align-items-center'>
+		<div className='item-tag-container d-flex justify-content-around align-items-center'>
 			<p>Item</p>
-			<div className='line' />
-			<div className='line' />
+			<img src={cross} alt='cross' height='25px' width='25px' />
 		</div>
 	);
 };
