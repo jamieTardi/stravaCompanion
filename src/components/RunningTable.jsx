4fe1ebc33bf8dd8cbb71a0694ca9f1b1938f01c3
jamie tardi <66372574+jamieTardi@users.noreply.github.com>
2021-05-20 08:54:43 +0100

    creating running only table from filter

--- src/components/RunningTable.jsx
+++ src/components/RunningTable.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Table } from 'react-bootstrap';
 
 const RunningTable = ({ filterRunning, setFilterRunning, activities }) => {
 	const handleRunningFilter = () => {
@@ -16,7 +17,39 @@ const RunningTable = ({ filterRunning, setFilterRunning, activities }) => {
 			console.log(e);
 		}
 	}, [activities]);
-	return <div></div>;
+	return (
+		<div>
+			<Table striped bordered hover variant='dark'>
+				<thead>
+					<tr>
+						<th>Date of Activity</th>
+						<th>Duration</th>
+						<th>Name of Activity</th>
+						<th>Distance Ran</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td>@mdo</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td>@fat</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td colSpan='2'>Larry the Bird</td>
+						<td>@twitter</td>
+					</tr>
+				</tbody>
+			</Table>
+		</div>
+	);
 };
 
 export default RunningTable;
